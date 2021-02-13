3ab1becf862bb6002f21f56c04f438e8d13d8f67
DavidPSchreiber <61207583+DavidPSchreiber@users.noreply.github.com>
2021-02-13 10:56:38 -0800

    JS begins

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('Test');
+
+
+// on page load, name field should be in focus
+
+const name = document.getElementById('name'); 
+    name.focus();
+
+const jobRole = document.getElementById('title');
+const otherJobRole = document.getElementById('other-job-role');
+// other-job-role is hidden by default
+otherJobRole.hidden = true;
