34dc45432088c032e5e8c630c93d20fa6f1d2717
DavidPSchreiber <61207583+DavidPSchreiber@users.noreply.github.com>
2021-02-14 11:03:18 -0800

    started JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 
-
 // on page load, name field should be in focus
 
 const name = document.getElementById('name'); 
@@ -7,5 +6,42 @@ const name = document.getElementById('name');
 
 const jobRole = document.getElementById('title');
 const otherJobRole = document.getElementById('other-job-role');
+
 // other-job-role is hidden by default
 otherJobRole.hidden = true;
+
+// need to add an event listener to job field
+// if 'other' is selected, make otherJobRole field visible; otherwise, keep hidden
+jobRole.addEventListener( 'change', (e) => {
+    if (e.target.value === 'other') {
+        otherJobRole.hidden = false;
+    }
+});
+
+// T-SHIRT SECTION
+
+//  user selects t-shirt design
+const tShirtDesign = document.getElementById('design');
+
+// design then gives select color options depending on the the design selected
+const tShirtColor = document.getElementById('color');
+const colorOptions = tShirtColor.children; 
+
+//  color is disabled until design selected
+tShirtColor.disabled = true;
+
+// add event listener to tShirtDesign selector
+tShirtDesign.addEventListener ( 'change', (e) => {
+
+// once design is chosen, color choice is activated
+tShirtColor.disabled = false;
+
+// use for-loop to provide color options per design choice
+for (let i = 0; i < colorOptions.length; i++) {
+    if (e.target.value === 'js puns') {
+        color
+    }
+
+}
+
+})
